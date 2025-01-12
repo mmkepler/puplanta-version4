7bef105036c1c6fa2945f9576202390bc1ce65f9
M. Kepler <mmkepler@yahoo.com>
2025-01-11 18:28:58 -0700

    feat: basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Link from 'next/link';
 import "../styles/footer.css";
 
 export default function Footer() {
+
+  const date: Date = new Date();
+  const year: number =  date.getFullYear();
   return (
-    <div>Footer</div>
+    <footer>
+      <p>©{year} Melissa Kepler </p>
+      <p><Link href="https://www.mmkepler.com">mmkepler.com</Link></p>
+    </footer>
   )
 }
