51bb2a3b34e9eabfbbb940188b9d9e27a91bd352
M. Kepler <mmkepler@yahoo.com>
2025-02-22 14:47:35 -0700

    fix: font issue on header/footer fixed, added info to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ export default function Footer() {
   const year = currentDate.getFullYear();
   return (
     <footer id="footer">
-      <p>© {year} Puplanta <a href="https://mmkepler.com" target="_blank">Melissa Kepler</a></p>
+      <span>© {year} Puplanta</span>
+      <span><a href="https://mmkepler.com" target="_blank">created by Melissa Kepler</a></span>
     </footer>
   )
 }
