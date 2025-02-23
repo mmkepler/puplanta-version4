27fbd70725820dfe4bd6e1292275f4363799c144
M. Kepler <mmkepler@yahoo.com>
2025-02-22 18:42:16 -0700

    feat: data sent fromstores/parks to individual pages on click

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,15 @@ import About from './components/About';
 import Account from './components/Account';
 import Home from './components/Home';
 import Parks from './components/Parks';
+import Park from "./components/Park";
 import Stores from './components/Stores';
+import Store from "./components/Store"
 import Header from './components/Header'
 import Footer from './components/Footer'
 import "./styles/App.css"
 
 
-
-
 function App() {
-
-
   return (
     <>
       <Header/>
@@ -23,8 +21,10 @@ function App() {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/about" element={<About/>}/>
                 <Route path="/parks" element={<Parks/>}/>
+                <Route path="/parks/:id" element={<Park/>}/>
                 <Route path="/stores" element={<Stores/>}/>
                 <Route path="/account" element={<Account/>}/>
+                <Route path="/stores/:id" element={<Store/>}/>
               </Routes>
             </main>
       <Footer/>
--- src/components/Parks.jsx
+++ src/components/Parks.jsx
@@ -1,6 +1,7 @@
 
 import supabase  from "../lib/supabase"
 import { useEffect, useRef, useState, } from "react";
+import { useNavigate } from "react-router-dom";
 import Map from "./Map";
 import  "../styles/parks.css"
 import { Link } from "react-router-dom";
@@ -8,13 +9,19 @@ import { Link } from "react-router-dom";
 
 export default function Parks() {
   const [parks, setParks] = useState([]);
+  const navigate = useNavigate();
   let popupData = [];
   let parkIndex;
+
   async function getParks() {
     const { data } = await supabase.from("parks").select();
-    
     setParks(data);
   }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    navigate("/parks/" + el.id, {test: "test"});
+  };
 
     useEffect(() => {
       getParks();
@@ -26,14 +33,14 @@ export default function Parks() {
     parks.forEach((item) => {
       let temp = {};
       let tempIndex = item.address.indexOf(",") + 1;
-      console.log("index ", tempIndex);
+      //console.log("index ", tempIndex);
       temp.position = [item.lat, item.lng];
       temp.name = item.title;
       temp.image = item.image;
       temp.address = item.address;
       temp.address1 = item.address.slice(0,tempIndex);
       temp.address2 = item.address.slice(tempIndex);
-      console.log("temp address1 ", temp.address1);
+      //console.log("temp address1 ", temp.address1);
       temp.id = item.id;
       popupData.push(temp);
     });
@@ -53,7 +60,7 @@ export default function Parks() {
               <p>{el.address.slice(0, el.address.indexOf(",") + 1)}</p>
               <p>{el.address.slice(el.address.indexOf(",") + 1)}</p>
               <p>
-                <Link to={"/parks/" + el.id}>Visit Park's Page to vote!</Link>
+                <Link to={ `/parks/${el.id}`} state={{data: el}}>Visit Park's Page to vote!</Link>
               </p>
             </li>)}
           </ul>
@@ -61,3 +68,14 @@ export default function Parks() {
       </div>
     );
 }
+
+/*<p>
+  <Link to={ `/parks/${el.id}`} state={{test: "test"}}>Visit Park's Page to vote!</Link>
+</p> */
+
+/*
+<p onClick={() => navigate(`/parks/${el.id}`, {test: "test"})}>
+                
+                Visit Park's Page to vote!
+              </p>
+*/
